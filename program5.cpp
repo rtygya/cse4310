26a9475a2f38c1e597db0bb7f17106809f6047cc
rtygya <103693384+rtygya@users.noreply.github.com>
2023-08-01 12:00:06 -0500

    Update and rename program5/cv_template_matching.cpp to program5.cpp

--- program5.cpp
+++ program5.cpp
@@ -1,24 +1,7 @@
-//
-//    Copyright 2018 Christopher D. McMurrough
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-
 /*******************************************************************************************************************//**
- * @file cv_template_matching.cpp
+ * @file program5.cpp
  * @brief C++ example for template matching with OpenCV
- * @author Christopher D. McMurrough
+ * @author Reety Gyawali
  **********************************************************************************************************************/
 
 // include necessary dependencies
@@ -30,13 +13,6 @@
 // configuration parameters
 #define NUM_COMNMAND_LINE_ARGUMENTS 2
 
-/*******************************************************************************************************************//**
- * @brief program entry point
- * @param[in] argc number of command line arguments
- * @param[in] argv string array of command line arguments
- * @return return code (0 for normal termination)
- * @author Christoper D. McMurrough
- **********************************************************************************************************************/
 int main(int argc, char **argv)
 {
     cv::Mat imageScene;
