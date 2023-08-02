fa3898f800bfd976c6063b60a3de0bca7c9e46fb
rtygya <103693384+rtygya@users.noreply.github.com>
2023-08-01 21:43:25 -0500

    Update program5.cpp

--- program5/program5.cpp
+++ program5/program5.cpp
@@ -1,6 +1,6 @@
 /*******************************************************************************************************************//**
  * @file program5.cpp
- * @brief C++ example for template matching with OpenCV
+ * @brief C++ example of simple screen scraping program using template matching with OpenCV
  * @author Reety Gyawali
  **********************************************************************************************************************/
 
@@ -12,72 +12,106 @@
 
 // configuration parameters
 #define NUM_COMNMAND_LINE_ARGUMENTS 2
+#define DISPLAY_WINDOW_NAME "Screen Scraper"
 
 int main(int argc, char **argv)
 {
-    cv::Mat imageScene;
     cv::Mat imageTemplate;
+    
+    // Store video capture parameters
+    std::string filename;
 
-    // validate and parse the command line arguments
+    // Validate and parse the command line arguments
     if(argc != NUM_COMNMAND_LINE_ARGUMENTS + 1)
     {
-        std::printf("USAGE: %s <scene_image> <template_image> \n", argv[0]);
+        std::printf("USAGE: %s <video> <template_image> \n", argv[0]);
         return 0;
     }
     else
     {
-        imageScene = cv::imread(argv[1], cv::IMREAD_COLOR);
+        filename = argv[1];
         imageTemplate = cv::imread(argv[2], cv::IMREAD_COLOR);
 
         // check for file error
-        if(!imageScene.data)
-        {
-            std::cout << "Error while opening file " << argv[1] << std::endl;
-            return 0;
-        }
         if(!imageTemplate.data)
         {
             std::cout << "Error while opening file " << argv[2] << std::endl;
             return 0;
         }
     }
+    
+    // Open the video file
+    cv::VideoCapture capture(filename);
+    if (!capture.isOpened()) {
+        std::cerr << "Unable to open video source!" << std::endl;
+        return 1;
+    }
 
-    // convert the images to grayscale
-    cv::Mat imageSceneGray;
-    cv::cvtColor(imageScene, imageSceneGray, cv::COLOR_BGR2GRAY);
+    // Create window
+    cv::namedWindow(DISPLAY_WINDOW_NAME, cv::WINDOW_AUTOSIZE);
+
+    //Show template and convert to grayscale
+    cv::namedWindow("Template", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Template", imageTemplate);
     cv::Mat imageTemplateGray;
     cv::cvtColor(imageTemplate, imageTemplateGray, cv::COLOR_BGR2GRAY);
 
-    // perform the matching step and normalize the searchResult
-    cv::Mat searchResult;
-    int match_method = CV_TM_CCORR_NORMED;
-    cv::matchTemplate(imageSceneGray, imageTemplateGray, searchResult, match_method);
-    cv::normalize(searchResult, searchResult, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+    /************************************************** START PROCESSING FRAMES *************************************************/
+    
+    cv::Mat frame;
 
-    // find the location of the best fit
-    double minVal;
-    double maxVal;
-    cv::Point minLocation;
-    cv::Point maxLocation;
-    cv::Point matchLocation;
-    cv::minMaxLoc(searchResult, &minVal, &maxVal, &minLocation, &maxLocation, cv::Mat());
-    if(match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED)
-    {
-        matchLocation = minLocation;
+    int frameCount = 0;
+  
+    while (1) {    
+        capture >> frame;
+
+        if (frame.empty())
+        {
+            break;
+        }
+        // convert frame to grayscale
+        cv::Mat frameGray;
+        cv::cvtColor(frame, frameGray, cv::COLOR_BGR2GRAY);
+    
+        // perform the matching step and normalize the searchResult
+        cv::Mat searchResult;
+        int match_method = CV_TM_CCORR_NORMED;
+        cv::matchTemplate(frameGray, imageTemplateGray, searchResult, match_method);
+        cv::normalize(searchResult, searchResult, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+    
+        // find the location of the best fit
+        double minVal;
+        double maxVal;
+        cv::Point minLocation;
+        cv::Point maxLocation;
+        cv::Point matchLocation;
+        cv::minMaxLoc(searchResult, &minVal, &maxVal, &minLocation, &maxLocation, cv::Mat());
+        if(match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED)
+        {
+            matchLocation = minLocation;
+        }
+        else
+        {
+            matchLocation = maxLocation;
+        }
+    
+        // annotate the scene image
+        cv::rectangle(imageScene, matchLocation, cv::Point(matchLocation.x + imageTemplate.cols , matchLocation.y + imageTemplate.rows), CV_RGB(0,255,0), 3);
+    
+        // display the frame
+        cv::imshow(DISPLAY_WINDOW_NAME, frame);
+        frameCount++;
+        std::cout << frameCount << std::endl;
+        
+        // Check for the 'q' key to quit the program
+        if ((char) cv::waitKey(1) == 'q') {
+            break;
+        }
     }
-    else
-    {
-        matchLocation = maxLocation;
-    }
 
-    // annotate the scene image
-    cv::rectangle(imageScene, matchLocation, cv::Point(matchLocation.x + imageTemplate.cols , matchLocation.y + imageTemplate.rows), CV_RGB(255,0,0), 3);
-
-    // display the images
-    cv::imshow("imageScene", imageScene);
-    cv::imshow("imageTemplate", imageTemplate);
-    cv::imshow("searchResult", searchResult);
-    cv::waitKey();
+    // Release the video capture and destroy windows
+    capture.release();
+    cv::destroyAllWindows();
 
     return 0;
 }
