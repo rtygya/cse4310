7b850207b520dd826dcd4cede93106be1503382a
rtygya <103693384+rtygya@users.noreply.github.com>
2023-08-06 13:24:11 -0500

    Update program4.cpp

--- program4/program4.cpp
+++ program4/program4.cpp
@@ -1,26 +1,10 @@
-//
-//    Copyright 2018 Christopher D. McMurrough
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
 /***********************************************************************************************************************
-* @file find_plane.cpp
+* @file program4.cpp
 * @brief finds a plane in a PCD file
 *
 * Simple example of loading and displaying PCD files with basic plane segmentation
 *
-* @author Christopher D. McMurrough
+* @author Reety Gyawali
 **********************************************************************************************************************/
 
 #include "CloudVisualizer.h"
